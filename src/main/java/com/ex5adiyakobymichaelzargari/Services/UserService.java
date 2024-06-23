87d73b6a1b0e9e947d48888ed6d08a48287101e0
Adi Yakoby <adija@edu.hac.ac.il>
2024-06-23 17:56:44 +0300

    feat

---
+++ src/main/java/com/ex5adiyakobymichaelzargari/Services/UserService.java
@@ -0,0 +1,28 @@
+package com.ex5adiyakobymichaelzargari.Services;
+
+
+import com.ex5adiyakobymichaelzargari.tabels.User;
+import com.ex5adiyakobymichaelzargari.tabels.UserRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/** Service layer is used to implement business logic
+ *  it is a good practice to separate the business logic from the controller
+ *  and the repository
+ *  this way the controller is only responsible for handling the request and response
+ *  the repository is only responsible for the database operations
+ *  and the service is responsible for the business logic
+ */
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+
+}
