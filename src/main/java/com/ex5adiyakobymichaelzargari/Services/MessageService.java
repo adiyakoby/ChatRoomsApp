3ceb6eb6042fed051ee0b8b1200b0e97ba7d2c95
Adi Yakoby <adija@edu.hac.ac.il>
2024-06-24 18:51:08 +0300

    feat - MessageService.java

---
+++ src/main/java/com/ex5adiyakobymichaelzargari/Services/MessageService.java
@@ -0,0 +1,40 @@
+package com.ex5adiyakobymichaelzargari.Services;
+
+import com.ex5adiyakobymichaelzargari.tabels.*;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MessageService {
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ChatRoomRepository chatRoomRepository;
+
+
+    public void registerNewMessage(String msg, String userName, String chatroomName) throws EntityNotFoundException {
+        ChatRoom chatRoom = chatRoomRepository.findByName(chatroomName);
+        User user = userRepository.findByUsername(userName);
+        if (chatRoom == null) {
+            throw new EntityNotFoundException("Chat Room Not Found");
+        } else if (user == null) {
+            throw new EntityNotFoundException("User Not Found");
+        }
+        Message message = new Message(msg, user, chatRoom);
+        messageRepository.save(message);
+    }
+
+
+    public List<Message> getAllMessages(String chatRoomName) {
+        ChatRoom chatRoom = chatRoomRepository.findByName(chatRoomName);
+        return chatRoom.getMessages();
+    }
+}
