97629d1d9219d45e348c5840db7f2dc5b01c9af0
Adi Yakoby <adija@edu.hac.ac.il>
2024-07-01 18:08:30 +0300

    fix - banned user cannot send msgs even if logged in already

--- src/main/java/com/ex5adiyakobymichaelzargari/webSocket/MessageController.java
+++ src/main/java/com/ex5adiyakobymichaelzargari/webSocket/MessageController.java
@@ -1,5 +1,6 @@
 package com.ex5adiyakobymichaelzargari.webSocket;
 
+import com.ex5adiyakobymichaelzargari.AppConstants;
 import com.ex5adiyakobymichaelzargari.Services.ChatRoomService;
 import com.ex5adiyakobymichaelzargari.tabels.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +12,14 @@ import org.springframework.stereotype.Controller;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+
 /**
  * MessageController handles the sending of messages in the websocket.
  */
 @Controller
 public class MessageController {
+
+
 
     @Autowired
     UserRepository userRepository;
@@ -30,34 +34,41 @@ public class MessageController {
     @Autowired
     private SimpMessagingTemplate messagingTemplate;
 
+
     /**
      * Sends a message to a chat room via the websocket.
      *
      * @param message the message to be sent
-     * @throws Exception if the message cannot be sent
      */
     @MessageMapping("/chat")
     @SendTo("/topic/messages")
-    public void send(MessageDTO message) throws Exception {
+    public void send(MessageDTO message) {
         try {
             String time = new SimpleDateFormat("HH:mm").format(new Date());
             User user = userRepository.findByUsername(message.getFrom());
             ChatRoom chatroom = chatRoomRepository.findById(message.getChatId()).orElse(null);
 
             if (chatroom == null || !chatroom.isEnabled()) {
-                String errorDestination = "/topic/errors/" + message.getFrom();
-                messagingTemplate.convertAndSend(errorDestination, "This chatroom was disabled or removed.");
+                String errorDestination = AppConstants.SOCKET_ERROR_PATH  + message.getFrom();
+                messagingTemplate.convertAndSend(errorDestination, AppConstants.SOCKET_ERR_MSG_DIS_REV);
+                return;
+            }
+
+            if (user.getRole().equals(AppConstants.ROLE_BANNED)) {
+                String errorDestination = AppConstants.SOCKET_ERROR_PATH  + message.getFrom();
+                messagingTemplate.convertAndSend(errorDestination, AppConstants.SOCKET_ERR_MSG_USER_BANNED);
                 return;
             }
 
             chatRoomService.addMessageToChatRoom(user, chatroom.getId(), message.getText(), time);
 
-            String destination = "/topic/chatroom/" + message.getChatId();
+            String destination = AppConstants.SOCKET_CHAT_PATH + message.getChatId();
             messagingTemplate.convertAndSend(destination , new MessageDTO(message.getFrom(), message.getText(), time));
         } catch (Exception e) {
-            String errorDestination = "/topic/errors/" + message.getFrom();
-            messagingTemplate.convertAndSend(errorDestination, "An error occurred while sending the message: " + e.getMessage());
+            String errorDestination = AppConstants.SOCKET_ERROR_PATH + message.getFrom();
+            messagingTemplate.convertAndSend(errorDestination, AppConstants.SOCKET_ERR_MSG_GENERAL + e.getMessage());
         }
 
     }
 }
+
