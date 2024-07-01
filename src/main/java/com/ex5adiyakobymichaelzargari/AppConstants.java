4923065f433e8ec9b7cea53cb7bd0f2af377441c
Adi Yakoby <adija@edu.hac.ac.il>
2024-07-01 18:08:15 +0300

    feat - AppConstants

---
+++ src/main/java/com/ex5adiyakobymichaelzargari/AppConstants.java
@@ -0,0 +1,11 @@
+package com.ex5adiyakobymichaelzargari;
+
+public class AppConstants {
+    public static final String SOCKET_ERROR_PATH = "/topic/errors/";
+    public static final String SOCKET_CHAT_PATH = "/topic/chatroom/";
+    public static final String SOCKET_ERR_MSG_DIS_REV = "This chatroom was disabled or removed.";
+    public static final String SOCKET_ERR_MSG_GENERAL = "This chatroom was disabled or removed.";
+    public static final String SOCKET_ERR_MSG_USER_BANNED = "You were banned, therefore you cannot send messages.";
+
+    public static final String ROLE_BANNED = "ROLE_BANNED";
+}
